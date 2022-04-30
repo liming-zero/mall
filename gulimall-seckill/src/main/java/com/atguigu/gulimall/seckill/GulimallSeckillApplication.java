2bcfa47c481e696643ecca09ef382286caa07459
liming <limingzero@outlook.com>
2022-05-01 00:23:09 +0800

    秒杀系统请求提交&拦截器

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/GulimallSeckillApplication.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/GulimallSeckillApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableRedisHttpSession
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
